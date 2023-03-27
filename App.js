ad7dfd65f1d354029d0676927a0f629b425f0ce6
tomasAybar <aybar.t4@gmail.com>
2023-03-27 14:40:21 -0300

    alert ubication

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
-import React, { useState } from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import MapView, { Marker, Polyline } from 'react-native-maps'
+import React, { useState, useEffect } from 'react'
+import * as Location from 'expo-location'
+import { StyleSheet, View } from 'react-native'
+import MapView, { Marker } from 'react-native-maps'
+import MapViewDirections from 'react-native-maps-directions'
+import { GOOGLE_MAPS_KEY } from '@env'
+import carImage from './assets/car.png'
 
 export default function App() {
-	const [origin, setOrigin] = useState({
-		latitude: -34.8014368557401,
-		longitude: -58.38758510301186,
-	})
+	useEffect(() => {
+		getLocationPermission()
+	}, [])
 
-	const [destination, setDestination] = useState({
+	const [origin, setOrigin] = useState({
 		latitude: -34.85161993160135,
 		longitude: -58.37984961256777,
 	})
 
-	// destino -34.85161993160135, -58.37984961256777
-	// origin -34.8014368557401, -58.38758510301186
+	const [destination, setDestination] = useState({
+		latitude: -34.8014368557401,
+		longitude: -58.38758510301186,
+	})
+
+	const getLocationPermission = async () => {
+		let { status } = await Location.requestForegroundPermissionsAsync()
+
+		if (status !== 'granted') {
+			alert('Permission to access location was denied')
+			return
+		}
+
+		let location = await Location.getCurrentPositionAsync()
+
+		setOrigin({
+			latitude: location.coords.latitude,
+			longitude: location.coords.longitude,
+		})
+	}
 
 	return (
 		<View style={styles.container}>
@@ -31,6 +52,7 @@ export default function App() {
 					draggable
 					coordinate={origin}
 					onDragEnd={(dir) => setOrigin(dir.nativeEvent.coordinate)}
+					image={carImage}
 				/>
 				<Marker
 					coordinate={destination}
@@ -38,10 +60,12 @@ export default function App() {
 					onDragEnd={(dir) => setDestination(dir.nativeEvent.coordinate)}
 				/>
 
-				<Polyline
-					coordinates={[origin, destination]}
-					strokeColor='blue'
-					strokeWidth={8}
+				<MapViewDirections
+					origin={origin}
+					destination={destination}
+					apikey={GOOGLE_MAPS_KEY}
+					strokeColor='black'
+					strokeWidth={5}
 				/>
 			</MapView>
 		</View>
